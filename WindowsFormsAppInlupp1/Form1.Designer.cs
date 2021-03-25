9d9bf4b80b06728d21c73da1d43e4d29abe3cbaf
mikaelaeriksson <mariamikaelaeriksson@gmail.com>
2021-03-25 16:00:53 +0100

    Added Assignment 1

---
+++ WindowsFormsAppInlupp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsAppInlupp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPris = new System.Windows.Forms.TextBox();
+            this.textBoxBetalt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPris
+            // 
+            this.textBoxPris.Location = new System.Drawing.Point(198, 45);
+            this.textBoxPris.Name = "textBoxPris";
+            this.textBoxPris.Size = new System.Drawing.Size(77, 20);
+            this.textBoxPris.TabIndex = 0;
+            this.textBoxPris.TextChanged += new System.EventHandler(this.textBoxPris_TextChanged);
+            // 
+            // textBoxBetalt
+            // 
+            this.textBoxBetalt.Location = new System.Drawing.Point(198, 93);
+            this.textBoxBetalt.Name = "textBoxBetalt";
+            this.textBoxBetalt.Size = new System.Drawing.Size(77, 20);
+            this.textBoxBetalt.TabIndex = 1;
+            this.textBoxBetalt.TextChanged += new System.EventHandler(this.textBoxBetalt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ange pris:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Betalt:";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(200, 171);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(141, 229);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(134, 110);
+            this.textBoxResult.TabIndex = 5;
+            this.textBoxResult.Text = "";
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 369);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBetalt);
+            this.Controls.Add(this.textBoxPris);
+            this.Name = "Form1";
+            this.Text = "Räknare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPris;
+        private System.Windows.Forms.TextBox textBoxBetalt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.RichTextBox textBoxResult;
+    }
+}
+
---
+++ WindowsFormsAppInlupp1/Form1.cs
@@ -0,0 +1,133 @@
+﻿// L0002B Inl 1
+// Mikaela Eriksson
+// Username: mikerv-0
+
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppInlupp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // When the ok button is clicked, the following code is executed
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            // Checks if the input for price and paid are valid (integer, not null and not empty)
+            if (!String.IsNullOrEmpty(textBoxPris.Text) && !String.IsNullOrEmpty(textBoxBetalt.Text) &&
+               int.TryParse(textBoxPris.Text, out _) && int.TryParse(textBoxBetalt.Text, out _))
+            {
+               
+                    int pris = Int32.Parse(textBoxPris.Text);
+                    int betalt = Int32.Parse(textBoxBetalt.Text);
+
+                    int total = betalt - pris;
+                    int total_vaxel = betalt - pris;
+
+                    if (betalt < pris)
+                    {
+                        textBoxResult.Text = "Du har betalat för lite. Det saknas " + total * (-1) + " kr.";
+                    }
+                    else if (betalt == pris)
+                    {
+                        textBoxResult.Text = "Tack, du har betalt tillräckligt!";
+                    }
+                    else if (betalt > pris)
+                    {
+
+                        var femhundra_var = "";
+                        var hundra_var = "";
+                        var femtio_var = "";
+                        var tjugo_var = "";
+                        var tio_var = "";
+                        var fem_var = "";
+                        var ett_var = "";
+
+                        int femhundra, hundra, femtio, tjugo, tio, fem, ett;
+
+                        while (total >= 500)
+                        {
+                            //Räknar antalet 500-lappar
+                            femhundra = total / 500;
+                            total = total % 500;
+                            femhundra_var = femhundra.ToString() + " Femhundralappar\r\n";
+
+                        }
+
+                        while (total >= 100)
+                        {
+                            //Räknar antalet 100-lappar
+                            hundra = total / 100;
+                            total = total % 100;
+                            //textBoxResult.Text = hundra + " Hundralappar\r\n";
+                            hundra_var = hundra.ToString() + " Hundralappar\r\n";
+                        }
+                        while (total >= 50)
+                        {
+                            //Räknar antalet 50-lappar
+                            femtio = total / 50;
+                            total = total % 50;
+                            femtio_var = femtio.ToString() + " Femtiolappar\r\n";
+                        }
+                        while (total >= 20)
+                        {
+                            //Räknar antalet 20-lappar
+                            tjugo = total / 20;
+                            total = total % 20;
+                            tjugo_var = tjugo.ToString() + " Tjugolappar\r\n";
+                        }
+                        while (total >= 10)
+                        {
+                            //Räknar antalet 10-kronor
+                            tio = total / 10;
+                            total = total % 10;
+                            tio_var = tio.ToString() + " Tiokronor\r\n";
+                        }
+                        while (total >= 5)
+                        {
+                            //Räknar antalet 5-kronor
+                            fem = total / 5;
+                            total = total % 5;
+                            fem_var = fem.ToString() + " Femkronor\r\n";
+                        }
+                        while (total >= 1)
+                        {
+                            //Räknar antalet 1-kronor
+                            ett = total / 1;
+                            total = total % 1;
+                            ett_var = ett.ToString() + " Enkronor\r\n";
+                        }
+
+                        // Counts the total change and which values
+                        textBoxResult.Text = "Växel tillbaka: " + total_vaxel + " kr" + "\n" +
+                        femhundra_var + hundra_var + femtio_var +
+                        tjugo_var + tio_var + fem_var + ett_var;
+                    }
+                
+
+            }
+            else
+                textBoxResult.Text = "Fyll i siffervärden för pris och betald summa!";
+        }
+
+        // Methods for the textbox fields
+        #region Textbox Methods
+        private void textBoxPris_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxBetalt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
